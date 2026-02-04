d875e10c040dc8c963dcf43c97e69afbae3dd8cd
gdiasmello <125673199+gdiasmello@users.noreply.github.com>
2026-02-03 23:49:37 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE = "artigiano-v1";
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["index.html", "styles.css", "app.js"])));
-});
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-});
